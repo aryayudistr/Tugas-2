3e3388fd1059c2140d7e828be893d744df940a70
ARYA <ARYA@Arya>
2018-09-12 13:08:21 +0700

    Tambah Hitung Fibonacci

--- src/hitungfibonacci/HitungFibonacci.java
+++ src/hitungfibonacci/HitungFibonacci.java
@@ -8,6 +8,8 @@ public class HitungFibonacci {
         tampilJudul(identitas);
         
         int n = tampilInput();
+        
+        BigInteger hasil = fibo(n);
     }
     
     private static void tampilJudul(String identitas)
@@ -26,6 +28,21 @@ public class HitungFibonacci {
         int n = scanner.nextInt();
         
         return n;
+    }
+    
+    private static BigInteger fibo(int n)
+    {
+        BigInteger[] hasil = new BigInteger[n];
+        
+        hasil[0] = BigInteger.ONE;
+        hasil[1] = BigInteger.ONE;
+        
+        for (int i = 2; i < n; i++)
+        {
+            hasil[i] = hasil[i-1].add(hasil[i-2]);
+        }
+        
+        return hasil[n-1];
     }
     
 }
