1184cd37d352af10026dbe95c0ca203a298633a9
ARYA <ARYA@Arya>
2018-09-14 20:17:35 +0700

    Proses

--- src/hitungfibonacci/Tugas.java
+++ src/hitungfibonacci/Tugas.java
@@ -14,5 +14,40 @@ public class Tugas {
         int o = 0, i = 0, j = 0, g = 0, m = 0;
         int[] y = new int[] {100, 500, 1000, 5000,};
         
+        while(n >= 5000)
+        {
+            n = n - 5000;
+            o++;
+        }
+        System.out.println("Banyak uang 5000 sebanyak : "+ o);
+        
+        while(n >= 2000)
+        {
+            n = n - 2000;
+            j++;
+        }
+        System.out.println("Banyak uang 2000 sebanyak : "+ j);
+        
+        while(n >= 1000)
+        {
+            n = n - 1000;
+            g++;
+        }
+        System.out.println("Banyak uang 1000 sebanyak : "+ g);
+        
+        while(n >= 500)
+        {
+            n = n - 500;
+            i++;
+        }
+        System.out.println("Banyak uang 500 sebanyak : "+ i);
+        
+        while(n >= 100)
+        {
+            n = n - 100;
+            m++;
+        }
+        System.out.println("Banyak uang 100 sebanyak : "+ m);
+        
 }
 }
