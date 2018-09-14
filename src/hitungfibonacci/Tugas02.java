83dc8417360ffaeaa9b43f6c680bed355722ebbe
ARYA <ARYA@Arya>
2018-09-14 20:22:00 +0700

    Deklarasi

--- src/hitungfibonacci/Tugas02.java
+++ src/hitungfibonacci/Tugas02.java
@@ -8,6 +8,13 @@ public class Tugas02 {
         System.out.println("Arya Yudistira / X RPL 3 / 08");
         
         
+        Scanner baca = new Scanner(System.in);
+        
+        System.out.print("Berat : ");
+        
+        int n = baca.nextInt();
+        double o=0, i=0, j=0, g=0, m=0, total, b=0;
+        double[] y = new double[]{4, 5, 2, 7, 3};
     }
     
 }
