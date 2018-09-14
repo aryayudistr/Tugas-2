62ef87f5a3364ebf6d3c69b9aa68c98149f9071f
ARYA <ARYA@Arya>
2018-09-14 20:16:05 +0700

    Initial Commit

---
+++ src/hitungfibonacci/Tugas.java
@@ -0,0 +1,10 @@
+
+package hitungfibonacci;
+import java.util.Scanner;
+public class Tugas {
+    public static void main(String[] args)
+    {
+        System.out.println("Arya Yudistira / X RPL 3 / 08");
+        
+}
+}
